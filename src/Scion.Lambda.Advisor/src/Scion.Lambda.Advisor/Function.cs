8a00747c3f6670d06a6a464eab78fa665ec43b58
Alex <alex_billson@outlook.com>
2023-04-24 14:54:09 +1000

    feat: Improved logging

--- src/Scion.Lambda.Advisor/src/Scion.Lambda.Advisor/Function.cs
+++ src/Scion.Lambda.Advisor/src/Scion.Lambda.Advisor/Function.cs
@@ -4,6 +4,7 @@ using Scion.Lambda.Common;
 using Scion.Lambda.Common.Interface.Models;
 using System.Text.Json;
 using System.Collections;
+using Microsoft.Extensions.Logging;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -20,11 +21,15 @@ namespace Scion.Lambda.Advisor
         /// <returns></returns>
         public async Task<IEnumerable<SetDetails>> FunctionHandler(FunctionInput input, ILambdaContext context)
         {
+            using var functionScope = Logger.BeginScope(input);
+
+            Logger.LogInformation("Querying sets");
             var setLists = await ExternalCardService.GetSetsAsync(new SetDetailsFilter
             {
                 After = input.After,
             });
 
+            Logger.LogInformation("{amount} of sets returned", setLists.Count());
             foreach (var set in setLists)
             {
                 await QueueOutputService.QueueSuccessAsync(set);
