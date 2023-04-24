e1cc79c9e9fe0d286c0cff1167daf79524324c71
Alex <alex_billson@outlook.com>
2023-04-24 15:24:09 +1000

    feat: Calling it there but making progress

---
+++ src/Scion.Lambda.Common.Interface/Service/Data/IChaliceRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scion.Lambda.Common.Interface.Service.Data
+{
+    public interface IChaliceRepository
+    {
+    }
+}
