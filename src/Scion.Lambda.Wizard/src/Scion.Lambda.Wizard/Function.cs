97dcdeb4b20d5f30ed781813e870efa2c4325d00
Alex <alex_billson@outlook.com>
2023-04-24 15:08:20 +1000

    feat: Some initial scaffolding of Wizard, need to do database design now

--- src/Scion.Lambda.Wizard/src/Scion.Lambda.Wizard/Function.cs
+++ src/Scion.Lambda.Wizard/src/Scion.Lambda.Wizard/Function.cs
@@ -1,22 +1,28 @@
 using Amazon.Lambda.Core;
+using Scion.Lambda.Common;
+using Scion.Lambda.Common.Interface.Models;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace Scion.Lambda.Wizard
 {
-    public class Function
+    public class Function : BaseFunction
     {
-
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public string FunctionHandler(FunctionInput input, ILambdaContext context)
         {
-            return input.ToUpper();
+            return input.Code;
+        }
+
+        public sealed class FunctionInput : SetDetails
+        {
+
         }
     }
 }
